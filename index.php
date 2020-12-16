0a2425a7540e59bcf344b4776b1e82777adb8023
Roberta <robertasciortino.ag@gmail.com>
2020-12-16 15:01:04 +0100

    stampa in html

--- index.php
+++ index.php
@@ -32,10 +32,39 @@
       "answer" => "In alcuni casi sì. Quando fai clic su un risultato della Ricerca Google, il tuo browser web potrebbe reindirizzare alla pagina web di destinazione anche l'indirizzo Internet, o URL, della pagina dei risultati di ricerca sotto forma di URL referrer. Talvolta, l'URL della pagina dei risultati di ricerca potrebbe contenere la query di ricerca che hai inserito. Se utilizzi la ricerca SSL (la funzione di ricerca criptata di Google), nella maggior parte dei casi i termini di ricerca non vengono inviati come parte dell'URL negli URL referrer. Questo comportamento può fare eccezione, ad esempio se utilizzi alcuni browser meno diffusi. Ulteriori informazioni sulla ricerca SSL sono disponibili qui. Le query di ricerca o le informazioni contenute nell'URL referrer potrebbero essere disponibili mediante Google Analytics o un'API (Application Programming Interface). Inoltre, gli inserzionisti potrebbero ricevere informazioni relative all' esatte parole chiave che hanno determinato il clic su un annuncio."
     ]
   ];
-
-  //stampa delle domande e delle risposte contenute nei sottoarray
-  foreach ($faq as $item) {
-    echo $item["question"];
-    echo $item["answer"];
-  }
 ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>PHP Google faq</title>
+  </head>
+  <body>
+    <header></header>
+
+    <main>
+      <div class="container">
+        <?php
+        foreach ($faq as $item) { //stampa delle domande e delle risposte contenute nei sottoarray
+        ?>
+          <div class="post">
+            <h2>
+              <?php
+              echo $item["question"];
+              ?>
+            </h2>
+            <p class="answer">
+              <?php
+              echo $item["answer"];
+              ?>
+            </p>
+          </div>
+        <?php
+        }
+        ?>
+      </div>
+    </main>
+
+    <footer></footer>
+  </body>
+</html>
